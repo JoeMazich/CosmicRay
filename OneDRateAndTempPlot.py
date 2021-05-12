a14b77fce7f1321e6de9234a82f2d80ec65bb195
Joe M <jmazich@luc.edu>
2021-05-12 10:05:42 -0500

    finished

--- OneDRateAndTempPlot.py
+++ OneDRateAndTempPlot.py
@@ -6,14 +6,23 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 inputdate = input('What date? ')
-det = input('What det? ')
+det1 = input('Det 1? ')
+det2 = input('Det 2? ')
+det3 = input('Det 3? ')
 
-earlytime = 80000
-latetime  = 100000
+earlytime  = '080000'
+latetime   = '100000'
+markedtime = '085000'
 
-file = open("DataDates/temp/" + inputdate + "/L0L1.txt", 'r')
-l0, temp = [], []
-times = []
+a = '080000'
+b = '083000'
+c = '090000'
+d = '093000'
+e = '100000'
+
+file = open("DataDates/" + inputdate + "/L0L1.txt", 'r')
+l01, l02, l03, temp1, temp2, temp3 = [], [], [], [], [], []
+times1, times2, times3 = [], [], []
 
 def Date_P2D(plain_date):
     year = 2000 + int(plain_date[0:2])
@@ -27,29 +36,88 @@ def Time_P2D(plain_time):
     sec = int(plain_time[4:6])
     return time(hour, min, sec)
 
+markedtime = datetime.combine(Date_P2D(inputdate), (Time_P2D(markedtime)))
+
+a = datetime.combine(Date_P2D(inputdate), (Time_P2D(a)))
+b = datetime.combine(Date_P2D(inputdate), (Time_P2D(b)))
+c = datetime.combine(Date_P2D(inputdate), (Time_P2D(c)))
+d = datetime.combine(Date_P2D(inputdate), (Time_P2D(d)))
+e = datetime.combine(Date_P2D(inputdate), (Time_P2D(e)))
+
 for line in file:
     columns = line.split()
 
-    if (columns[2] == det) and (int(columns[1]) > earlytime) and (int(columns[1]) < latetime):
-        l0.append(float(columns[3]))
-        temp.append(float(columns[8]))
-        times.append(datetime.combine(Date_P2D(columns[0]), (Time_P2D(columns[1]))))
+    if (int(columns[1]) >= int(earlytime)) and (int(columns[1]) <= int(latetime)):
+        if (columns[2] == det1):
+            l01.append(float(columns[3]))
+            temp1.append(float(columns[8]))
+            times1.append(datetime.combine(Date_P2D(columns[0]), (Time_P2D(columns[1]))))
+        elif (columns[2] == det2):
+            l02.append(float(columns[3]))
+            temp2.append(float(columns[8]))
+            times2.append(datetime.combine(Date_P2D(columns[0]), (Time_P2D(columns[1]))))
+        elif (columns[2] == det3):
+            l03.append(float(columns[3]))
+            temp3.append(float(columns[8]))
+            times3.append(datetime.combine(Date_P2D(columns[0]), (Time_P2D(columns[1]))))
 
 file.close()
 
-
 fig = plt.figure(constrained_layout=False)
 
-gs = fig.add_gridspec(2, 1, left=0.09, right=0.91, top=.87, bottom=0.09, wspace=0.4, hspace=0.5)
 
-l0ax = fig.add_subplot(gs[0, 0])
-l0ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%H:%M'))
-l0ax.plot(times, l0)
+gs = fig.add_gridspec(2, 3, left=0.09, right=0.91, top=.87, bottom=0.09, wspace=0.4, hspace=0.2)
+fig.suptitle(inputdate)
 
-tempax = fig.add_subplot(gs[1, 0])
-tempax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%H:%M'))
-tempax.plot(times, temp)
+l01ax = fig.add_subplot(gs[0, 0])
+l01ax.set_title(det1)
+l01ax.set_xticks([])
+l01ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%H:%M'))
+l01ax.plot(times1, l01)
+l01ax.plot([markedtime, markedtime], [0, 10000])
+l01ax.set_xlim([datetime.combine(Date_P2D(inputdate), (Time_P2D(earlytime))), datetime.combine(Date_P2D(inputdate), (Time_P2D(latetime)))])
+l01ax.set_ylim([735, 748])
 
+temp1ax = fig.add_subplot(gs[1, 0])
+temp1ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%H:%M'))
+temp1ax.plot(times1, temp1)
+temp1ax.plot([markedtime, markedtime], [0, 10000])
+temp1ax.set_xlim([datetime.combine(Date_P2D(inputdate), (Time_P2D(earlytime))), datetime.combine(Date_P2D(inputdate), (Time_P2D(latetime)))])
+temp1ax.set_xticks([a,b,c,d,e])
+temp1ax.set_ylim([17, 21])
 
+l02ax = fig.add_subplot(gs[0, 1])
+l02ax.set_title(det2)
+l02ax.set_xticks([])
+l02ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%H:%M'))
+l02ax.plot(times2, l02)
+l02ax.plot([markedtime, markedtime], [0, 10000])
+l02ax.set_xlim([datetime.combine(Date_P2D(inputdate), (Time_P2D(earlytime))), datetime.combine(Date_P2D(inputdate), (Time_P2D(latetime)))])
+l02ax.set_ylim([735, 748])
+
+temp2ax = fig.add_subplot(gs[1, 1])
+temp2ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%H:%M'))
+temp2ax.plot(times2, temp2)
+temp2ax.plot([markedtime, markedtime], [0, 10000])
+temp2ax.set_xlim([datetime.combine(Date_P2D(inputdate), (Time_P2D(earlytime))), datetime.combine(Date_P2D(inputdate), (Time_P2D(latetime)))])
+temp2ax.set_xticks([a,b,c,d,e])
+temp2ax.set_ylim([17, 21])
+
+l03ax = fig.add_subplot(gs[0, 2])
+l03ax.set_title(det3)
+l03ax.set_xticks([])
+l03ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%H:%M'))
+l03ax.plot(times3, l03)
+l03ax.plot([markedtime, markedtime], [0, 10000])
+l03ax.set_xlim([datetime.combine(Date_P2D(inputdate), (Time_P2D(earlytime))), datetime.combine(Date_P2D(inputdate), (Time_P2D(latetime)))])
+l03ax.set_ylim([735, 748])
+
+temp3ax = fig.add_subplot(gs[1, 2])
+temp3ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%H:%M'))
+temp3ax.plot(times3, temp3)
+temp3ax.plot([markedtime, markedtime], [0, 10000])
+temp3ax.set_xlim([datetime.combine(Date_P2D(inputdate), (Time_P2D(earlytime))), datetime.combine(Date_P2D(inputdate), (Time_P2D(latetime)))])
+temp3ax.set_xticks([a,b,c,d,e])
+temp3ax.set_ylim([17, 21])
 
 plt.show()
